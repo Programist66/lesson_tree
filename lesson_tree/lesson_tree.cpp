713d1223532c5699403eda4062d33a6ed5e05b05
Dmitriy Zolotuhin <zolotuhindima2006@gmail.com>
2024-01-25 15:13:25 +0300

    Добавьте файлы проекта.

---
+++ lesson_tree/lesson_tree.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+#include "Binaries_Tree.h"
+
+using namespace std;
+
+int main() 
+{
+	BinaryTree<int> numbers;
+	for (int i = 0; i < 10; i++)
+	{
+		int y = rand() % 100;
+		numbers.insert(y);
+	}
+	numbers.Print();
+	numbers.remove(58);
+	cout << "=========================" << endl;
+	numbers.Print();
+	return 0;
+}
